f99c93c1b09b22ea7625df38a7577cfc1d4597ee
sortafixed <128575362+sortafixed@users.noreply.github.com>
2023-09-19 01:50:41 -0500

    updated conftest

--- test/conftest.py
+++ test/conftest.py
@@ -5,14 +5,20 @@ from selenium.webdriver.common.by import By
 from utilities.test_data import TestData
 
 
-@pytest.fixture(params=["chrome"])
+@pytest.fixture(params=["chrome", "firefox", "edge"])
 def initialize_driver(request):
-    driver = webdriver.Chrome()
+    if request.param == "chrome":
+        driver = webdriver.Chrome()
+    elif request.param == "firefox":
+        driver = webdriver.Firefox()
+    elif request.param == "edge":
+        driver = webdriver.Edge()
     request.cls.driver = driver
     print(f"Browser: {request.param}")
     driver.get(TestData.url)
+    driver.maximize_window()
     yield
     print("Close Driver")
-    #driver.close()
+    driver.close()
 
 
