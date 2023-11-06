95ea553ed7f48ba4c33bf6cc44d46d053a977d6b
sortafixed <128575362+sortafixed@users.noreply.github.com>
2023-11-05 23:35:57 -0600

    keeping the ugly xpath

--- page_objects/dashboard_page.py
+++ page_objects/dashboard_page.py
@@ -5,7 +5,7 @@ from page_objects.base_page import BasePage
 
 class MyDashboard(BasePage):
     dd_user_menu = (By.ID, "dd-user-menu")
-    dd_user_menu_logout = (By.CLASS_NAME, ".float-right")
+    dd_user_menu_logout = (By.XPATH, "/html/body/header/div/div/div/div[1]/div[5]/div/a[6]")
 
 
 
